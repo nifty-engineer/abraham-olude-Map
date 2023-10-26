40a344a02d59e55a3ac3759c39cc33dcd28dd2bd
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-26 03:59:07 +0000

    Complete Coding Lab: Map

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,5 +1,6 @@
 
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -23,7 +24,8 @@ public class Lab {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+
+        return new TreeMap<>();
     }
 
     /**
@@ -33,7 +35,7 @@ public class Lab {
      */
     public int getSize(Map<Integer,String> map){
 //        return total number of key/value pairs in the map
-        return 0;
+        return map.size();
     }
 
     /**
@@ -44,6 +46,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+
+        map.put(key, value);
     }
 
     /**
@@ -53,7 +57,8 @@ public class Lab {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+
+        return map.get(key);
     }
 
     /**
@@ -63,6 +68,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+
+        map.remove(key);
     }
 
     /**
@@ -74,5 +81,6 @@ public class Lab {
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
 
+        map.replace(key, value);
     }
 }
